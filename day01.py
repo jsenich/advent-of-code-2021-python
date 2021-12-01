fb02779fd905241e7a66a1aa3acc1b933b44b67f
Jay Senich <john_senich@rapid7.com>
2021-12-01 00:30:31 -0500

    Day one exercises complete

---
+++ day01.py
@@ -0,0 +1,31 @@
+def part_one(puzzle_input: list[int]) -> int:
+    increasedCount = 0
+    for i, depth in enumerate(puzzle_input):
+        if i > 0:
+            if depth > puzzle_input[i - 1]:
+                increasedCount += 1
+
+    return increasedCount
+
+
+def part_two(puzzle_input: list[int]) -> int:
+    increasedCount = 0
+
+    for i in range(len(puzzle_input)):
+        if i > 0 and i + 2 < len(puzzle_input):
+            if (
+                sum([puzzle_input[i], puzzle_input[i + 1], puzzle_input[i + 2]]) >
+                sum([puzzle_input[i - 1], puzzle_input[i], puzzle_input[i + 1]])
+            ):
+                increasedCount += 1
+
+    return increasedCount
+
+
+if __name__ == '__main__':
+
+    with open('data/day01_input.txt') as f:
+        puzzle_input = [int(n) for n in f.readlines()]
+
+    print(f'part one answer: {part_one(puzzle_input)}')
+    print(f'part one answer: {part_two(puzzle_input)}')
