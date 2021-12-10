0b629f79e031641d71efefb58fd41519bd352614
Jay Senich <1821282+jsenich@users.noreply.github.com>
2021-12-10 10:30:19 -0500

    Day 10 complete

---
+++ day10.py
@@ -0,0 +1,104 @@
+from collections import deque
+from copy import copy
+
+
+CLOSING_MAP = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>',
+}
+
+OPENING_CHARACTERS = CLOSING_MAP.keys()
+
+
+def part_one(puzzle_input: str) -> int:
+    score_map: dict[str, int] = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137,
+    }
+    score = 0
+    for line in puzzle_input.splitlines():
+        chars = list(line)
+        opening_chars = deque()
+        for i, c in enumerate(chars):
+            if c in OPENING_CHARACTERS:
+                opening_chars.append(c)
+            else:
+                if c != CLOSING_MAP[opening_chars[-1]]:
+                    score += score_map[c]
+                    break
+                else:
+                    opening_chars.pop()
+
+    return score
+
+
+def part_two(puzzle_input: str) -> int:
+    score_map = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4,
+    }
+    scores = []
+    puzzle_lines = puzzle_input.splitlines()
+    incomplete_lines = copy(puzzle_lines)
+    for line in puzzle_lines:
+        chars = list(line)
+        opening_chars = deque()
+        for i, c in enumerate(chars):
+            if c in OPENING_CHARACTERS:
+                opening_chars.append(c)
+            else:
+                if c != CLOSING_MAP[opening_chars[-1]]:
+                    incomplete_lines.remove(line)
+                    break
+                else:
+                    opening_chars.pop()
+
+    for line in incomplete_lines:
+        chars = list(line)
+        opening_chars = deque()
+        for i, c in enumerate(chars):
+            if c in OPENING_CHARACTERS:
+                opening_chars.append(c)
+            else:
+                opening_chars.pop()
+
+        closing = list(reversed([CLOSING_MAP[c] for c in opening_chars]))
+
+        line_score = 0
+
+        for c in closing:
+            line_score = (line_score * 5) + score_map[c]
+
+        scores.append(line_score)
+
+    scores = sorted(scores)
+
+    return scores[int((len(scores) - 1) / 2)]
+
+
+if __name__ == '__main__':
+
+    puzzle_input = """\
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+"""
+
+    with open('data/day10_input.txt') as f:
+        puzzle_input = f.read().strip()
+
+    print(f'part one answer: {part_one(puzzle_input)}')
+    print(f'part two answer: {part_two(puzzle_input)}')
